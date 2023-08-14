0d767bb9e9c4fd1fbdc107e9ebd0b3699721e9db
Bladimir Parra - IP Proyectos y Soluciones <bladyparra@gmail.com>
2023-08-14 01:13:30 -0500

    Configuración de bodyParser, cors,

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express, { json } from 'express';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import cors from 'cors';
+import bodyParser from 'body-parser'
 import indexRoute from './routes/index.js';
 import morgan from 'morgan';
 
@@ -9,14 +10,27 @@ import morgan from 'morgan';
 
 const server = express();
 
-// server.use( morgan( 'dev' ) );
-// server.use( json() );
-// server.use( cors() );
+server.use( morgan( 'dev' ) );
+server.use( json() );
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 server.set('views', join(__dirname, 'views'));
 server.set('view engine', 'ejs');
+
+const corsOptions = {
+  origin: '*',
+  methods: '*',
+};
+
+server.use( cors( corsOptions ) );
+server.use( bodyParser.json() );
+server.use( bodyParser.urlencoded( { extended: false } ) );
+
+server.use( ( req, res, next ) => {
+  console.log( `${ req.url } -${ req.method }` );
+  next();
+});
 
 server.use('/', indexRoute);
 server.use(express.static(join(__dirname, 'public')));
