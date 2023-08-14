330e69335202fedbdb0e85c3435ea26d61fcf988
Bladimir Parra - IP Proyectos y Soluciones <bladyparra@gmail.com>
2023-08-14 01:15:40 -0500

    Configuración de rutas

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -6,5 +6,29 @@ router.get('/', (req, res) => res.render(
   'index', { title: 'Hotel Portobelo | Backend' }
 ));
 
+router.get('/index', (req, res) => res.render(
+  'index', { title: 'Hotel Portobelo | Backend' }
+));
+
+router.get('/administrators', (req, res) => res.render(
+  'administrators', { title: 'Hotel Portobelo | Backend - Administrators' }
+));
+
+router.get('/banner', (req, res) => res.render(
+  'banner', { title: 'Hotel Portobelo | Backend - Banner' }
+));
+
+router.get('/login', (req, res) => res.render(
+  'login', { title: 'Hotel Portobelo | Backend - Login' }
+));
+
+router.get('/plans', (req, res) => res.render(
+  'plans', { title: 'Hotel Portobelo | Backend - Plans' }
+));
+
+router.get('/bookings', (req, res) => res.render(
+  'bookings', { title: 'Hotel Portobelo | Backend - Bookings' }
+));
+
 
 export default router;
