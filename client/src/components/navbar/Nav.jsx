109bc5a72833fb45ab158d6ff9aa2638987a9955
EmiCastro1997 <casacastro97@gmail.com>
2023-08-16 00:31:54 -0300

    primer commit

---
+++ client/src/components/navbar/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () =>{
+    return(
+        <div className="navbar">
+            <div>
+            <button onClick="">RESERVA</button>
+            </div>
+            <div>
+                <button onClick="">LOGIN</button>
+            </div>
+        </div>
+    )
+}
+export default Nav;
