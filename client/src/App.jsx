fd69881224c1ba78a466f05853d08d50e5f88045
EmiCastro1997 <casacastro97@gmail.com>
2023-08-16 00:31:54 -0300

    primer commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-
+import Nav from "./components/navbar/nav";
 function App() {
 
   return (
     <div>
-      <h1>Hello Wolrd!!</h1>
+      <h1 className="text-3xl font-bold underline">Hello Wolrd!!</h1>
+      <Nav/>
     </div>
   )
 }
