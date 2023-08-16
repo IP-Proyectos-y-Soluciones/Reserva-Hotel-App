bdff9cbeb456dbbe69ab08249b28ce8e51987fbc
Bladimir Parra - IP Proyectos y Soluciones <bladyparra@gmail.com>
2023-08-16 03:34:15 -0500

    Fix del db.js, creación de la DB, y de las relaciones

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -8,31 +8,38 @@ const {
 } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DATABASE}`, {
-  logging: false, 
-  native: false, 
+  logging: false,
+  native: false,
 });
 
-const basename = path.basename(__filename);
+const __filename = fileURLToPath( import.meta.url );
+const __dirname = path.dirname(__filename);
 
 const modelDefiners = [];
 
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+fs.readdirSync(path.join(__dirname, 'models'))
+  .filter((file) => (file.indexOf('.') !== 0) && (file.slice(-3) === '.js'))
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+    const model = await import(path.join(__dirname, 'models', file));
+    modelDefiners.push(model.default);
   });
 
-modelDefiners.forEach(model => model(sequelize));
+modelDefiners.forEach((modelDefiner) => modelDefiner(sequelize));
 
-let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+const entries = Object.entries(sequelize.models);
+const capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-const {  } = sequelize.models;
-// Aca vendrian las relaciones
+const { Diary, Bookings, Testimonials, Bedrooms, Users } = sequelize.models;
 
+Diary.belongsTo(Bedrooms, { foreignKey: 'id_room' });
+Bookings.belongsTo(Users, { foreignKey: 'id_user' });
+Bookings.belongsTo(Bedrooms, { foreignKey: 'id_room' });
+Testimonials.belongsTo(Users, { foreignKey: 'id_res' });
+Testimonials.belongsTo(Users, { foreignKey: 'id_us' });
+Testimonials.belongsTo(Bedrooms, { foreignKey: 'id_room' });
 
-module.exports = {
-  ...sequelize.models, 
+export default {
+  ...sequelize.models,
   conn: sequelize,
 };
