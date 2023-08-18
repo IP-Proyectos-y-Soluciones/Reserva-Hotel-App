41afc74821b069d925b06a16fe282c31afa92862
juan boglione <boglionejuan33@gmail.com>
2023-08-17 21:53:45 -0300

    conexiones en app.jsx y cards en Landing2 xd

--- client/src/views/Landing.jsx
+++ client/src/views/Landing.jsx
@@ -1,10 +1,11 @@
-import Cards from "../components/Cards";
+import Cards from "../components/Cards/Cards";
 
 
 
 const Landing = () => {
     return (
         <div>
+            <h1>Landing</h1>
             <Cards />
         </div>
     )
