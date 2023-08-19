8cc220abfc574392132e422ffaec0fb8e89483af
juan boglione <boglionejuan33@gmail.com>
2023-08-18 23:39:23 -0300

    datos del lado izquierdo de la nav renderizados faltaria rutearlos

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -26,34 +26,49 @@ const NavBar = () =>{
             </span>
           </button>
           
-          <div className="!visible hidden flex-grow basis[100%] items-center lg:!flex lg:basis-auto"
-          id="navbarSupportedContent1"
-          data-te-collapse-item >
-          
-          {/* <a
-          className="flex items-center mt-3 mb-4 mr-2 text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:mb-0 lg:mt-0"
+          <div
+        className="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto"
+        id="navbarSupportedContent1"
+        data-te-collapse-item>
+        {/* <a className="flex items-center mt-3 mb-4 mr-2 text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:mb-0 lg:mt-0"
           href="#">
           <img
-            src={"https://tecdn.b-cdn.net/img/logo/te-transparent-noshadows.webp"}
+            src="https://tecdn.b-cdn.net/img/logo/te-transparent-noshadows.webp"
             style="height: 15px"
             alt=""
-            loading="lazy"
-            />
+            loading="lazy" />
         </a> */}
-            
-            <ul 
-              className="flex-col pl-0 mr-auto list-style-none lg:flex-row"
-              data-te-navbar-nav-ref>
-              <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-              <a 
-              className="text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400" 
+        <ul className="flex flex-col pl-0 mr-auto list-style-none lg:flex-row"
+          data-te-navbar-nav-ref>
+          <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+            <a
+              className="text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-zinc-400"
               href="#"
-              data-te-env-nav-link-ref
-              >habitaciones |</a>
-                
-              </li>
-                
-              </ul>
+              data-te-nav-link-ref
+              >Habitaciones</a>
+          </li>
+          <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+            <a
+              className="text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+              href="#"
+              data-te-nav-link-ref
+              >Planes</a>
+          </li>
+          <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+            <a
+              className="text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+              href="#"
+              data-te-nav-link-ref
+              >Ameneties</a>
+          </li>
+          <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
+            <a
+              className="text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+              href="#"
+              data-te-nav-link-ref
+              >Contacto</a>
+          </li>
+        </ul>
           </div>
         </div>
       </nav>
