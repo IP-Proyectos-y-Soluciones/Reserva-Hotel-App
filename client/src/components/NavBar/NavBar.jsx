062b2a53b94983a6ab780087938af3f5f63a8bfe
juan boglione <boglionejuan33@gmail.com>
2023-08-18 22:29:58 -0300

    responsive navBar hamburguesa en vista

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,14 +1,31 @@
 
 const NavBar = () =>{
     return(
-        <div className="navbar">
-            <div>
-            <button onClick="">RESERVA</button>
-            </div>
-            <div>
-                <button onClick="">LOGIN</button>
-            </div>
+      //main 
+      <nav className="relative flex flex-no-wrap items-center justify-between w-full py-2 shadow-md bg-neutral-100 shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-4" data-te-navbar-ref>
+        <div className="flex items-end justify-between px-3 w-full-wrap">
+          <button
+          className="block px-2 bg-transparent border-0 text bg-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-one focus:ring-0 dark:text-neutral-200 lg:hidden"
+            type="button"
+            data-te-collapse-init
+            data-te-target="#navBarSupportedContent1"
+            aria-controls="navBarsupportedContent1"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="[&>svg]:w-7">
+            <svg
+            xmlns="https://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="h-7 w-7">
+            <path fillRule="evenodd"
+              d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+              clipRule="evenodd"/>
+            </svg>
+            </span>
+          </button>
         </div>
+      </nav>
     )
 }
 export default NavBar;
