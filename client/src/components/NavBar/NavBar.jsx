336d74283c04950d6db56a0dd7a06c91e934f548
juan boglione <boglionejuan33@gmail.com>
2023-08-19 22:44:09 -0300

    renderizado de nav cards en landing, nav aplicado poco y nada de dise;o sin nada routeado

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,77 +1,33 @@
 
+
 const NavBar = () =>{
     return(
-      //main 
-      <nav className="relative flex flex-no-wrap items-center justify-between w-full py-2 shadow-md bg-neutral-100 shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-4" data-te-navbar-ref>
-        <div className="flex items-end justify-between px-3 w-full-wrap">
-        
-          <button
-          className="block px-2 bg-transparent border-0 text bg-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-one focus:ring-0 dark:text-neutral-200 lg:hidden"
-            type="button"
-            data-te-collapse-init
-            data-te-target="#navBarSupportedContent1"
-            aria-controls="navBarsupportedContent1"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="[&>svg]:w-7">
-            <svg
-            xmlns="https://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="h-7 w-7">
-            <path fillRule="evenodd"
-              d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-              clipRule="evenodd" />
-            </svg>
-            </span>
-          </button>
-          
-          <div
-        className="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto"
-        id="navbarSupportedContent1"
-        data-te-collapse-item>
-        {/* <a className="flex items-center mt-3 mb-4 mr-2 text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:mb-0 lg:mt-0"
-          href="#">
-          <img
-            src="https://tecdn.b-cdn.net/img/logo/te-transparent-noshadows.webp"
-            style="height: 15px"
-            alt=""
-            loading="lazy" />
-        </a> */}
-        <ul className="flex flex-col pl-0 mr-auto list-style-none lg:flex-row"
-          data-te-navbar-nav-ref>
-          <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-            <a
-              className="text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-zinc-400"
-              href="#"
-              data-te-nav-link-ref
-              >Habitaciones</a>
-          </li>
-          <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-            <a
-              className="text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-              href="#"
-              data-te-nav-link-ref
-              >Planes</a>
-          </li>
-          <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-            <a
-              className="text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-              href="#"
-              data-te-nav-link-ref
-              >Ameneties</a>
-          </li>
-          <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-            <a
-              className="text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-              href="#"
-              data-te-nav-link-ref
-              >Contacto</a>
-          </li>
-        </ul>
+        <div className= "bg-blue-500" >
+          <ul>
+          <div className="flex-row">
+            <li>
+            habitaciones
+            </li>
+            <li>
+              Planes
+            </li>
+            <li>
+              Servicios
+            </li>
+            <li>
+              Contacto
+            </li>
           </div>
+          <div className="flex-row-reverse">
+            <li>
+              Reserva
+            </li>
+            <li>
+              Login
+            </li>
+          </div>
+          </ul>
         </div>
-      </nav>
     )
 }
 export default NavBar;
--- client/src/views/Landing.jsx
+++ client/src/views/Landing.jsx
@@ -1,11 +1,16 @@
-import NavBar from "../components/NavBar/NavBar"
+import NavBar from "../components/NavBar/NavBar";
+import CardsPlan from "../components/CardPlan/CardPlan";
+import CardsBedroom from "../components/CardsBedroom/CardsBedroom";
+// import CardsAmeneties from "../components/CardsAmenitie/CardsAmenitie";
 
 const Landing = () => {
     return (
         <div>
-                <header>
-                    <NavBar />
-                </header>
+            <NavBar />
+            <h5 className="bg-red-500">Banner</h5>
+            <CardsPlan />
+            <CardsBedroom />
+            {/* <CardsAmeneties /> */}
         </div>
     )
 }
