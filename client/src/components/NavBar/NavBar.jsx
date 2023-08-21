9622a107d64ded50bd2a033337f20b301172ea6c
Danylopgali <daniellopg@hotmail.com>
2023-08-20 20:35:36 -0600

    navbar arreglada

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,36 +2,25 @@
 
 const NavBar = () =>{
     return(
-        <div className= "bg-blue-500" >
-          <ul>
-          <div className="flex space-x-4 parte1 1">
-            <li>
-            habitaciones
-            </li>
-            <li>
-              Planes
-            </li>
-            <li>
-              Servicios
-            </li>
-            <li>
-              Contacto
-            </li>
-          </div>
-          <div className="flex flex-row-reverse w-6/4">
-            <div className="flex-row-reverse p-4">
-            <li >
-              Reserva
-            </li>
-            </div>
-            <div className="p-4">
-            <li>
-              Login
-            </li>
-            </div>
-          </div>
-          </ul>
+      <div className="bg-blue-500">
+      <ul className="flex items-center justify-between"> 
+        <div className="flex space-x-4 ">
+          <li>Habitaciones</li>
+          <li>Planes</li>
+          <li>Servicios</li>
+          <li>Contacto</li>
         </div>
+        <div className="flex space-x-4 ">
+          <div className="flex items-center">
+            <li>Reserva</li>
+          </div>
+          <div className="flex items-center">
+            <li>Login</li>
+          </div>
+        </div>
+      </ul>
+    </div>
+     
     )
 }
 export default NavBar;
