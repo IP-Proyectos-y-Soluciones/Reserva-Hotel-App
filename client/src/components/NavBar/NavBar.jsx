b4f47bf3d42b717b2f3b5deea51f0ac9392c2161
juan boglione <boglionejuan33@gmail.com>
2023-08-21 20:20:10 -0300

    cambios de rutas en app, navBar y login

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 const NavBar = () =>{
@@ -5,17 +6,30 @@ const NavBar = () =>{
       <div className="bg-blue-500">
       <ul className="flex items-center justify-between"> 
         <div className="flex space-x-4 ">
+          <Link to="/">
+          <li>Home</li>
+          </Link>
+          <Link>
           <li>Habitaciones</li>
+          </Link>
+          <Link>
           <li>Planes</li>
+          </Link>
+          <Link>
           <li>Servicios</li>
+          </Link>
+          <Link>
           <li>Contacto</li>
+          </Link>
         </div>
         <div className="flex space-x-4 ">
           <div className="flex items-center">
             <li>Reserva</li>
           </div>
           <div className="flex items-center">
+            <Link to="/users/login" >
             <li>Login</li>
+            </Link>
           </div>
         </div>
       </ul>
