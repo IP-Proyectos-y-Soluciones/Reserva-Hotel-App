5e63689be1278887142675a9d2d9a53d2bf9b50f
juan boglione <boglionejuan33@gmail.com>
2023-08-21 20:33:59 -0300

    cambios en el diselño de la landing

--- client/src/views/Landing.jsx
+++ client/src/views/Landing.jsx
@@ -5,12 +5,20 @@ import CardsBedroom from "../components/CardsBedroom/CardsBedroom";
 
 const Landing = () => {
     return (
-        <div>
+        <div className="min-h-screen bg-gray-100">
             <NavBar />
-            <h5 className="bg-red-500">Banner</h5>
-            <CardsPlan />
-            <CardsBedroom />
-            {/* <CardsAmeneties /> */}
+            <h5 className="py-2 text-center text-white bg-red-500">Banner</h5>
+            <div className="container flex flex-row p-4 mx-auto space-x-4">
+                <div className="flex-grow">
+                    <CardsPlan />
+                </div>
+                <div className="flex-grow">
+                    <CardsBedroom />
+                </div>
+                {/* <div className="flex-grow">
+                    <CardsAmenities />
+                </div> */}
+            </div>
         </div>
     )
 }
