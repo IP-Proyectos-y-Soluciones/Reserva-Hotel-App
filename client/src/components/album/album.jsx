ca25543b394df9ea80a0357a7632262345c67254
Danylopgali <daniellopg@hotmail.com>
2023-08-23 17:00:19 -0600

    album funcional desde DB

--- client/src/components/album/album.jsx
+++ client/src/components/album/album.jsx
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Album = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [images, setImages] = useState([]);
 
-  const images = ['https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Japanese_youth_hostel_room.jpg/330px-Japanese_youth_hostel_room.jpg', 'https://images.homify.com/image/upload/c_scale,h_375,w_500/v1439365783/p/photo/image/489918/Depto_DL_kababie_arquitectos_20.jpg']; // Agrega más nombres de imagen si es necesario
+  useEffect(() => {
+    // Realiza una solicitud a la API para obtener los datos de las habitaciones
+    axios.get('http://localhost:3001/bedroom')
+      .then(response => {
+        const bedrooms = response.data;
+        const galleryImages = bedrooms[0].gallery || []; // Suponiendo que solo obtendrás el primer registro
+        setImages(galleryImages);
+      })
+      .catch(error => {
+        console.error('Error fetching images:', error);
+      });
+  }, []);
 
   const showSlide = (index) => {
     setCurrentSlide(index);
@@ -23,9 +36,9 @@ const Album = () => {
 
   return (
     <div>
-      <h1>Este es el Album </h1>
+      <h1>Este es el Album</h1>
       
-      <div className="flex items-center justify-center ">
+      <div className="flex items-center justify-center">
         <div className="w-full overflow-hidden bg-white rounded-lg shadow-lg md:w-2/3 lg:w-1/2 xl:w-1/3">
           <div className="relative">
             <div id="slider" className="flex">
