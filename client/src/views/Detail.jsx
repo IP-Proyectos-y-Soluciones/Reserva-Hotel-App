b2c5790555a14dbcdd862dd9d1edb3080b8791a6
Danylopgali <daniellopg@hotmail.com>
2023-08-23 21:42:24 -0600

    link to detail no funcional

--- client/src/views/Detail.jsx
+++ client/src/views/Detail.jsx
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 import Album from "../components/album/album";
 
 function Detail() {
   const { id } = useParams();
   const [roomData, setRoomData] = useState({});
-  const [plans, setPlans] = useState([]); // State for plans
+  const [roomList, setRoomList] = useState([]);
+  const [plans, setPlans] = useState([]);
 
   useEffect(() => {
+    axios.get(`http://localhost:3001/bedroom`)
+      .then(response => {
+        const roomListData = response.data;
+        setRoomList(roomListData);
+      })
+      .catch(error => {
+        console.error('Error fetching room list:', error);
+      });
+
     axios.get(`http://localhost:3001/bedroom/detail/${id}`)
       .then(response => {
         const selectedRoomData = response.data;
@@ -23,7 +33,6 @@ function Detail() {
         console.error('Error fetching room data:', error);
       });
 
-    // Fetch plans from the API
     axios.get('http://localhost:3001/plan')
       .then(response => {
         const fetchedPlans = response.data;
@@ -38,26 +47,31 @@ function Detail() {
     <div className="flex h-screen detail">
       <div className="grid flex-1 gap-4 p-4 auto-rows-min">
         <div className="imagen&type bg-gray-100 p-4">
-          <h2 className=""> Caribeña / Colonial / Hindú / Marroquí / Retro </h2>
+          <h2 className="">
+            {roomList.map(room => (
+              <Link key={room.id} to={`/detail/${room.id}`}>
+                {room.kind_h}{' / '}
+              </Link>
+             
+            ))}
+          </h2>
           <div className="w-full mx-auto md:w-2/3 lg:w-1/2 xl:w-1/3">
-            <Album id={id} /> {/* Cambio a minúscula: { id } */}
+            <Album id={id} />
           </div>
         </div>
         <div className="p-4 bg-gray-200 description">
-        <h2>{roomData.description_h}</h2>
+          <h2>{roomData.description_h}</h2>
         </div>
         <div className="p-4 bg-gray-300 description-of-plan">
           <div className="grid grid-cols-2 gap-4">
-          <div>
-              {plans.map(plan => (
-                <div key={plan.kind}>
-                  <h2>{plan.kind}</h2>
-                  <p>{plan.description}</p>
-                  <p>Precio alto: {plan.hight_price}</p>
-                  <p>Precio bajo: {plan.low_price}</p>
-                </div>
-              ))}
-            </div>
+            {plans.map(plan => (
+              <div key={plan.kind}>
+                <h2>{plan.kind}</h2>
+                <p>{plan.description}</p>
+                <p>Precio alto: {plan.hight_price}</p>
+                <p>Precio bajo: {plan.low_price}</p>
+              </div>
+            ))}
             <div className="w-20 iconos " >
               <img src="imagen-de-iconos-de-servicios" alt="img de servicios" />
               <h2> Aquí está lo que incluye cada plan </h2>
