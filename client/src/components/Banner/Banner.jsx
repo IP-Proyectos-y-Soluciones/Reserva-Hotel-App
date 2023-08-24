04369132a9d21d8909a5e7f83c8c0800411d347c
KariRomero <karinaromero.1993@gmail.com>
2023-08-24 15:06:44 -0300

    create slide Banner

--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -1,17 +1,53 @@
-// import imgHall1 from '../../assets/hall/1.jpg';
-import imgHall2 from '../../assets/hall/2.jpg';
-// import imgHall3 from '../../assets/hall/3.jpg';
+import { useState } from 'react';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 
-const Banner = () => {
-    return(
-        <div>
 
-            
-            {/* <img src={imgHall1} alt='Ingreso al hotel'/> */}
-            <img src={imgHall2} alt='Ingreso al hotel'/>
-            {/* <img src={imgHall3} alt='Ingreso al hotel'/> */}
-        </div>
-    )
+
+const Banner = () => {  
+
+  const slides =[
+    {url:'https://res.cloudinary.com/dm9glx5a7/image/upload/v1692147833/PF-HOTEL-APP/HALL/3_gubg3f.jpg'
+  },
+    {url:'https://res.cloudinary.com/dm9glx5a7/image/upload/v1692147833/PF-HOTEL-APP/HALL/2_tg4jxp.jpg'
+  },
+    {url:'https://res.cloudinary.com/dm9glx5a7/image/upload/v1692147833/PF-HOTEL-APP/HALL/1_pxxkn1.jpg'
+  }
+  ];
+
+  const [currentIndex,setCurrentIndex] = useState(2);
+
+  const prevSlide =()=>{
+    const isFirstSlide = currentIndex === 0;
+    const newIndex= isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide =()=>{
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  
+
+  return (
+  
+  <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
+
+    <div style={{backgroundImage:`url(${slides[currentIndex].url})`}}className='w-full h-full bg-center bg-cover duration-500'
+    ></div>
+
+    <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+    <BsChevronCompactLeft onClick={prevSlide} size={38}/>
+    </div>
+
+    <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+    <BsChevronCompactRight onClick={nextSlide} size={38}/>
+    </div>
+  </div>) 
+
+    
+  
 };
 
 export default Banner;
