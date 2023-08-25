19bc48b5e1f5b3f6d56139c69a547cfb71bc5f75
KariRomero <karinaromero.1993@gmail.com>
2023-08-25 02:00:42 -0300

    hover CardBedroom

--- client/src/components/CardBedroom/CardBedroom.jsx
+++ client/src/components/CardBedroom/CardBedroom.jsx
@@ -2,17 +2,27 @@ import { Link } from 'react-router-dom';
 
 const CardBedroom = ({id_h, gallery, kind_h}) =>{
     return (
-                     
-        <div className="max-w-[400px]">
+        
             
+        <div
+        class="relative max-w-xs overflow-hidden bg-cover bg-no-repeat"
+        data-te-ripple-init
+        data-te-ripple-color="light">
             <Link to={`/detail/${id_h}`}> 
-            <p>{kind_h}</p>
-            <img src={gallery} alt={kind_h} className="w-100 h-85"/>
-            </Link>
             
-            
+        <img
+          src={gallery}
+          class="max-w-xs"
+          alt={kind_h} />
+        <a href="#!">
+          <div
+            class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+        </a>
+        </Link> 
+      </div>
+      
 
-        </div>
+       
     )
 }
 export default CardBedroom;
