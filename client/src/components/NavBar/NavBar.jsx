cd7956bd89c1a226f8546af5fad13b7a204b04dc
juan boglione <boglionejuan33@gmail.com>
2023-08-25 20:50:04 -0300

    colores en tailwind y dise;o de la nav, falta linkearla

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 const NavBar = () =>{
     return(
-      <div className="bg-blue-500">
-      <ul className="flex items-center justify-between"> 
-        <div className="flex space-x-4 ">
+      <div className="bg-[#313131] shadow align-middle" >
+      <ul className="flex items-center justify-between text-[#B99768]"> 
+        <div className="flex space-x-4 font-normal">
           <Link to="/">
-          <li>Home</li>
+          <li> Home</li>
           </Link>
           <Link>
-          <li>Habitaciones</li>
+          <li> Habitaciones</li>
           </Link>
           <Link>
           <li>Planes</li>
@@ -23,10 +23,10 @@ const NavBar = () =>{
           </Link>
         </div>
         <div className="flex space-x-4 ">
-          <div className="flex items-center">
+          <div className="bg-[#B99768] text-white px-4">
             <li>Reserva</li>
           </div>
-          <div className="flex items-center">
+          <div className="px-4 bg-[#B99768] text-white">
             <Link to="/users/login" >
             <li>Login</li>
             </Link>
