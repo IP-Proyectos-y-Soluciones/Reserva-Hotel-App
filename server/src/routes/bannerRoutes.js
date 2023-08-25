0f90994b0e324ccd6f818d321f305aa79164435e
hasanenginkubat <kubathasanengin@gmail.com>
2023-08-24 23:42:48 -0500

    ejs falta poco

--- server/src/routes/bannerRoutes.js
+++ server/src/routes/bannerRoutes.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const { createBanner, updateBanner, getBanner } = require('../controllers/bannerControllers');
+const { createBanner, updateBanner, getBanner, deleteBanner } = require('../controllers/bannerControllers');
 
 router.post('/', async (req, res) => {
   try {
     const { img } = req.body;
     const result = await createBanner(img);
-    res.json(result);
+    const banners = await getBanner()
+    res.render('pages/banner.ejs', {  result, banners, title: 'Hotel Backend' })
+    //res.status(201).json(result);
   } catch (error) {
+    console.error(error)
     res.status(500).json({ error: error.message });
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.post('/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const { img, date } = req.body;
-    const result = await updateBanner(id, img, date);
-    res.json(result);
+    const { img } = req.body;
+    const banners = await getBanner()
+    const result = await updateBanner(id, img);
+    res.render('pages/banner.ejs', {  result, banners, title: 'Hotel Backend' })
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -25,11 +29,29 @@ router.put('/:id', async (req, res) => {
 
 router.get('/', async (req, res) => {
   try {
-    const result = await getBanner();
-    res.json(result);
+    const banners = await getBanner();
+    res.render('pages/banner.ejs', {  banners, title: 'Hotel Backend' })
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
+
+router.post('/delete/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await deleteBanner(id);
+
+    if (result.error) {
+      res.status(404).json({ error: result.error });
+    } else {
+      const banners = await getBanner();
+      res.render('pages/banner.ejs', { banners, result, title: 'Hotel Backend' })
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 
 module.exports = router;
