6b3d69c59d9f32048484b4d61883b78ab9eaf095
juan boglione <boglionejuan33@gmail.com>
2023-08-25 20:59:15 -0300

    mas cambios en la nav

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -3,31 +3,33 @@ import { Link } from "react-router-dom";
 
 const NavBar = () =>{
     return(
-      <div className="bg-[#313131] shadow align-middle" >
+      <div className="bg-[#313131] shadow align-middle text-lg ">
       <ul className="flex items-center justify-between text-[#B99768]"> 
         <div className="flex space-x-4 font-normal">
-          <Link to="/">
+          <Link to="/" className="hover:underline">
           <li> Home</li>
           </Link>
-          <Link>
+          <Link className="hover:underline" >
           <li> Habitaciones</li>
           </Link>
-          <Link>
+          <Link className="hover:underline">
           <li>Planes</li>
           </Link>
-          <Link>
+          <Link className="hover:underline">
           <li>Servicios</li>
           </Link>
-          <Link>
+          <Link className="hover:underline">
           <li>Contacto</li>
           </Link>
         </div>
         <div className="flex space-x-4 ">
           <div className="bg-[#B99768] text-white px-4">
+          <Link to="/Booking" className="hover:underline">
             <li>Reserva</li>
+            </Link>
           </div>
           <div className="px-4 bg-[#B99768] text-white">
-            <Link to="/users/login" >
+            <Link to="/users/login" className="hover:underline">
             <li>Login</li>
             </Link>
           </div>
