740d330aa86479550fc687858648b1391a2191e9
KariRomero <karinaromero.1993@gmail.com>
2023-08-25 23:58:43 -0300

    fix urlCloudinary, style Cards

--- client/src/components/CardPlan/CardPlan.jsx
+++ client/src/components/CardPlan/CardPlan.jsx
@@ -2,11 +2,8 @@ const CardPlan = ({onMouseEnter, onMouseLeave, img, kind}) =>{
     
     return (
                     
-        <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} className="max-w-[400px]">             
-            
-            <p>{kind}</p>
-            <img src={img} alt={kind} className="w-100 h-85"/>                
-
+        <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>             
+            <img src={img} alt={kind} className="w-60 h-40" />                
         </div>
     )
 }
