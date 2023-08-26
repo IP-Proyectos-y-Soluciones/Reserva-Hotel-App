5d34673952699622b52d8e04714af6e5b9123402
KariRomero <karinaromero.1993@gmail.com>
2023-08-26 01:27:36 -0300

    style cardPlan

--- client/src/components/CardPlan/CardPlan.jsx
+++ client/src/components/CardPlan/CardPlan.jsx
@@ -2,9 +2,18 @@ const CardPlan = ({onMouseEnter, onMouseLeave, img, kind}) =>{
     
     return (
                     
-        <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>             
-            <img src={img} alt={kind} className="w-60 h-40" />                
-        </div>
+        // <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>             
+        //     <img src={img} alt={kind} className="w-60 h-40" />                
+        // </div>
+
+
+<div>
+<figure class="relative max-w-sm transition-all duration-300 cursor-pointer " onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+   <a href="#">
+     <img class="rounded-lg" src={img} alt={kind}/>
+   </a>     
+</figure>
+</div>
     )
 }
 export default CardPlan;
