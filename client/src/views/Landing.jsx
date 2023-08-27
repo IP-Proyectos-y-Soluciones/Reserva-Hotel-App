7e63b3b7e25547c324444c246cdeb2421f8a2813
KariRomero <karinaromero.1993@gmail.com>
2023-08-27 18:17:56 -0300

    br entre componentes

--- client/src/views/Landing.jsx
+++ client/src/views/Landing.jsx
@@ -22,34 +22,50 @@ const Landing = () => {
 
     return (
         <div className="min-h-screen bg-[#585552]">
+
             <NavBar />
-            
             <Banner/>
+
+            <br></br>
+            <br></br>
+
                 <div className="leading-10 tracking-widest text-center">
                 <div className="py-2 text-4xl font-semibold tracking-widest font">
                     <h2 className="text-[#B99768]">BIENVENIDOS</h2>
                 </div>
                 <div className="py-4 mx-3 my-2 text-3xl font-light leading-10 tracking-wider">
                     <h3 className="text-white">Descubran un refugio de lujo en el corazón de la ciudad. Con elegantes habitaciones, comodidades de primer nivel y un servicio excepcional, estamos aquí para hacer de su estadía una experiencia inolvidable. Desde relajarse en nuestro spa hasta disfrutar de deliciosas opciones gastronómicas, les invitamos a sumergirse en un mundo de tranquilidad y comodidad. ¡Es un placer tenerles aquí!</h3>
+                </div>               
                 </div>
-                </div>
+
+                <br></br>
+                <br></br>
+
             <div>
                 <div className="px-5 text-left text-[#B99768] text-4xl tracking-widest font-semibold">
                     <h1>Descubre la magia del lugar</h1>
                 </div>
+                <br></br>
                     <div className='flex items-center justify-center'>
                         <CardsPlan onCardHover={handleCardHover} onCardLeave={handleCardLeave} />
                     </div>
             </div>
 
+            <br></br>
+            <br></br>
+        
             <div>
                 <div className="text-4xl font-semibold tracking-widest text-right text-[#B99768] px-5">
                     <h1>El confort que mereces</h1>
                 </div>
+                
                     <div className='flex items-center justify-center'>
                         <CardsBedroom />
                     </div>
             </div>
+
+            <br></br>
+            <br></br>
                 
                 <div className='flex items-center justify-center'>
                     <CardsAmenitie />
@@ -58,7 +74,7 @@ const Landing = () => {
             <Footer />
             <ButtonBackToTop />
             {hoveredCard && (
-        <div className="fixed bottom-0 left-0 right-0 bg-white p-4 text-center">
+        <div className="fixed bottom-0 left-0 right-0 bg-[#B99768] p-4 text-center">
           <p className="text-black">{hoveredCard}</p>
         </div>
       )}
