37842a2f548b052284e36d955c55032228bf9fc6
KariRomero <karinaromero.1993@gmail.com>
2023-08-27 17:30:05 -0300

    style cardsPlan prueba

--- client/src/views/Landing.jsx
+++ client/src/views/Landing.jsx
@@ -5,49 +5,71 @@ import CardsAmenitie from "../components/CardsAmenitie/CardsAmenitie";
 import Banner from "../components/Banner/Banner";
 import Footer from "../components/Footer/Footer";
 import ButtonBackToTop from "../components/ButtonBackToTop/ButtonBackToTop";
+import { useState } from 'react';
 
 const Landing = () => {
-    return (
+    
+    const [hoveredCard, setHoveredCard] = useState(null);
+    
 
+    const handleCardHover = (description) => {
+        setHoveredCard(description);
+    };
+
+    const handleCardLeave = () => {
+        setHoveredCard(null);
+    };
+
+    return (
         <div className="min-h-screen bg-[#585552]">
-            <NavBar />
-            
-            <Banner/>
+            <NavBar/>
+
+            <Banner />
             <div>
                 <div className="font-semibold leading-10 tracking-widest text-center text-zinc-800">
-            <h2 className="text-white">BIENVENIDOS</h2>
-            <h3 className="text-3x1">Descubran un refugio de lujo en el corazón de la ciudad. Con elegantes habitaciones, comodidades de primer nivel y un servicio excepcional, estamos aquí para hacer de su estadía una experiencia inolvidable. Desde relajarse en nuestro spa hasta disfrutar de deliciosas opciones gastronómicas, les invitamos a sumergirse en un mundo de tranquilidad y comodidad. ¡Es un placer tenerles aquí!</h3>
+                    <h2 className="text-white">BIENVENIDOS</h2>
+                    <h3 className="text-3x1">Descubran un refugio de lujo en el corazón de la ciudad. Con elegantes habitaciones, comodidades de primer nivel y un servicio excepcional, estamos aquí para hacer de su estadía una experiencia inolvidable. Desde relajarse en nuestro spa hasta disfrutar de deliciosas opciones gastronómicas, les invitamos a sumergirse en un mundo de tranquilidad y comodidad. ¡Es un placer tenerles aquí!</h3>
                 </div>
             </div>
 
+            
+
             <div>
-                <div >
+                <div>
                     <h1>Descubre la magia del lugar</h1>
                 </div>
-                    <div className='flex justify-center items-center'>
-                        <CardsPlan />
-                    </div>
+                 
+                <div className='flex justify-center items-center'>
+                    <CardsPlan
+                        onCardHover={handleCardHover}
+                        onCardLeave={handleCardLeave}
+                    />
+                </div>
             </div>
 
             <div>
                 <div>
                     <h1>El confort que mereces</h1>
                 </div>
-                    <div className='flex justify-center items-center'>
-                        <CardsBedroom />
-                    </div>
-            </div>
-                
-                <div className='flex justify-center items-center'>
-                    <CardsAmenitie />
+                <div ref={ref} className='flex justify-center items-center'>
+                    <CardsBedroom />
                 </div>
+            </div>
+
+            <div className='flex justify-center items-center'>
+                <CardsAmenitie />
+            </div>
 
             <Footer />
-            <ButtonBackToTop/>
-
-
+            <ButtonBackToTop />
+            {hoveredCard && (
+        <div className="fixed bottom-0 left-0 right-0 bg-white p-4 text-center">
+          <p className="text-black">{hoveredCard}</p>
+        </div>
+      )}
+            
         </div>
     );
 }
 
-export default Landing
+export default Landing;
