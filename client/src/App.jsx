7bfb1a15acbf9614389690099c4d389f6780e2d2
KariRomero <karinaromero.1993@gmail.com>
2023-08-28 15:43:38 -0300

    detail cambios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import NavBar from "./components/NavBar/NavBar";
 import Landing from "./views/Landing";
 import Detail from "./views/Detail";
 import Booking from "./components/Booking/Booking";
-import Login from "./components/login/Login";
+// import Login from "./components/login/Login";
 import CardBedroom from "./components/CardBedroom/CardBedroom";
 import { Route, Routes, useLocation } from "react-router-dom";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
@@ -22,7 +22,7 @@ const App = () => {
         <Route path="/bedroom" element={<CardBedroom />} />
         {/* <Route path="/detail/:id" element={<Detail />} /> */}
         <Route path="/booking" element={<Booking />} />
-        <Route path="/users/login" element={<Login />} />
+        {/* <Route path="/users/login" element={<Login />} /> */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
