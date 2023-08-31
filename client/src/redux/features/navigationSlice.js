2a1ded1e7ffc9ddb76438f0e45d722fd62316eaa
juan boglione <boglionejuan33@gmail.com>
2023-08-31 18:10:03 -0300

    arreglo en el footer.ejs

---
+++ client/src/redux/features/navigationSlice.js
@@ -0,0 +1,10 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+
+// const initialState = {
+//     showHomeLink: true,
+//     showLoginLink: true,
+//     showBookingLink: false,
+// };
+
+// const navigationSlice = createSlice({})
