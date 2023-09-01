db013bea39dba95528b0a75fdb251223a3065081
hasanenginkubat <kubathasanengin@gmail.com>
2023-09-01 13:59:56 -0500

    redu

--- client/src/redux/features/userSlice.js
+++ client/src/redux/features/userSlice.js
@@ -57,12 +57,6 @@ const usersSlice = createSlice({
       .addCase(loginUser.fulfilled,(state, action)=>{
         state.userLogin=action.payload;
       })
-      .addCase(loginUser.fulfilled, (state, action) => {
-        state.userLogin = {
-          ...action.payload,
-          id: action.payload.map(user => user.id)
-        };
-      })
       
       .addCase(updatedsUser.fulfilled, (state, action) => {
         state.userData = action.payload;
