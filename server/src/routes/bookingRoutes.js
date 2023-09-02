aec289c6ba61c42e166050140f9b1e1cde536af8
Bladimir Gerson Parra Bermudez <bladyparra@gmail.com>
2023-09-02 04:00:53 +0000

    Update Bookings route
    Signed-off-by: GitHub <noreply@github.com>

--- server/src/routes/bookingRoutes.js
+++ server/src/routes/bookingRoutes.js
@@ -96,7 +96,8 @@ router.put('/:id_reservation', async (req, res) => {
               <p style="color: white; font-size: 20px; margin: 0;">${text}</p>
           </div>`, // html body
     });
-
+    
+      console.log("Message sent: %s", info.messageId);
 
     res.status(200).json(result);
   } catch (error) {
