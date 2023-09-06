208929b2203742c337423529ebd94838ff8f96d5
juan boglione <boglionejuan33@gmail.com>
2023-09-06 01:18:37 -0300

    prueba

--- client/src/redux/actions/bannerActions.js
+++ client/src/redux/actions/bannerActions.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const urlBannner = "http://localhost:3001/banner";
+const urlBannner = "/banner";
 
 export const getAllBanner=createAsyncThunk(
     "banner/allBanner",
