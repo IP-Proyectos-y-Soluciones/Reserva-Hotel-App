88f77d45548ea22a144eb207ab1a0c1d3a3aa493
Bladimir Parra - IP Proyectos y Soluciones <bladyparra@gmail.com>
2023-09-06 21:40:43 -0500

    bug fixes deploy

--- server/src/app.js
+++ server/src/app.js
@@ -18,27 +18,27 @@ const DB_INTERNAL_URL = process.env;
 config();
 const server = express();
 
-// server.set("trust proxy", true);
+server.set("trust proxy", true);
 
 // Configurar el limitador de velocidad: máximo de cinco solicitudes por minuto
-// const limiter = RateLimit( {
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   max: 100, // limitar cada IP a 100 solicitudes por ventana
-// } );
+const limiter = RateLimit( {
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limitar cada IP a 100 solicitudes por ventana
+} );
 
 // Aplicar limitador de tasa a todas las solicitudes
-// server.use( limiter );
+server.use( limiter );
 
 server.use( morgan( 'dev' ) );
 server.use( express.json() );
 
 //:::::::::::AGREGADO X::::::::::::::::::::::::::::
 server.use((req, res, next) => {
-  //res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Origin', 'https://reserva-hotel-app-ipproyectosysoluciones.vercel.app/');
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
