6b26ff66dad0698a4dd313069d9ccdc923e151ac
Danylopgali <daniellopg@hotmail.com>
2023-09-06 20:17:38 -0600

    testimonial Form contrlado y con diseño

--- client/src/views/TestimonialForm.jsx
+++ client/src/views/TestimonialForm.jsx
@@ -1,90 +1,4 @@
-// import React, { useState, useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { postTestimonials } from "../redux/actions/testimonialsActions";
-// import { getBedroom } from "../redux/actions/bedroomsActions";
 
-// const TestimonialForm = () => {
-//   const dispatch = useDispatch();
-//   const [testimonial, setTestimonial] = useState({
-//     testimony: "",
-//     bedroomId: "", 
-//   });
-
-//   const { bedrooms } = useSelector((state) => state.bedrooms);
-
-//   useEffect(() => {
-    
-//     dispatch(getBedroom());
-//   }, [dispatch]);
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setTestimonial({
-//       ...testimonial,
-//       [name]: value,
-//     });
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-
-//     try {
-      
-//       await dispatch(postTestimonials(testimonial));
-
-      
-//       setTestimonial({
-//         testimony: "",
-//         bedroomId: "",
-//       });
-
-   
-//     } catch (error) {
-//       console.error("Error al agregar el testimonio:", error);
-      
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h2>Agregar Testimonio</h2>
-//       <form onSubmit={handleSubmit}>
-//         <div>
-//           <label htmlFor="testimony">Testimonio:</label>
-//           <textarea
-//             name="testimony"
-//             id="testimony"
-//             value={testimonial.testimony}
-//             onChange={handleChange}
-//             required
-//           />
-//         </div>
-//         <div>
-//           <label htmlFor="bedroomId">Habitación:</label>
-//           <select
-//             name="bedroomId"
-//             id="bedroomId"
-//             value={testimonial.bedroomId}
-//             onChange={handleChange}
-//             required
-//           >
-//             <option value="">Seleccione una habitación</option>
-//             {bedrooms.map((room) => (
-//               <option key={room.id} value={room.id}>
-//                 {room.kind_h}
-//               </option>
-//             ))}
-//           </select>
-//         </div>
-//         <div>
-//           <button type="submit">Agregar Testimonio</button>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default TestimonialForm;
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { postTestimonials } from "../redux/actions/testimonialsActions";
@@ -96,6 +10,7 @@ const TestimonialForm = () => {
     testimony: "",
     bedroomId: "",
   });
+  const [isSubmitSuccess, setIsSubmitSuccess] = useState(false); // Estado para el mensaje de éxito
 
   const { bedrooms } = useSelector((state) => state.bedrooms);
 
@@ -115,7 +30,6 @@ const TestimonialForm = () => {
     e.preventDefault();
 
     try {
-    
       const testimonialToSend = {
         ...testimonial,
         approved: false,
@@ -127,6 +41,9 @@ const TestimonialForm = () => {
         testimony: "",
         bedroomId: "",
       });
+
+      // Mostrar el mensaje de éxito
+      setIsSubmitSuccess(true);
     } catch (error) {
       console.error("Error al agregar el testimonio:", error);
     }
@@ -135,6 +52,9 @@ const TestimonialForm = () => {
   return (
     <div className="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md">
       <h2 className="mb-4 text-xl font-bold">Agregar Testimonio</h2>
+      {isSubmitSuccess && (
+        <div className="mb-4 font-bold text-green-600">Testimonio enviado con éxito.</div>
+      )}
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
           <label
